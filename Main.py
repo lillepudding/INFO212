dbf8d4cb8261c9e1d0ee9a40d3082a018e830eff
Henrik <henrik.ivers@gmail.com>
2025-09-30 11:06:28 +0200

    movies -> main

--- Main.py
+++ Main.py
@@ -3,32 +3,36 @@
 movies = {}
 
 class Movie:
-    def __init__(self, name, image):
-        self.name = name
-        self.image = image
-        movies[name] = self
+    def __init__(self, title, year, director, description, poster_path):
+        self.title = title
+        self.year = year
+        self.director = director
+        self.description = description
+        self.poster_path = poster_path
     
     def info_string(self):
-        return self.name+","+self.image
+        return self.title+","+str(self.year)+","+self.director+","+self.description+","+self.poster_path
 
 
 def save_movies():
-    with open("info212\movies.txt", "w") as m:
+    with open("info212/movies.txt", "w") as m:
         for movie in movies:
             m.write(movies[movie].info_string()+"\n")
 
 
 def load_movies():
-    with open("info212\movies.txt") as m:
-        movie_list = m.read().split("\n")[:-1]
+    with open("info212/movies.txt") as mo:
+        movie_list = mo.read().split("\n")[:-1]
+        print(movie_list)
         for movie in movie_list:
             features = movie.split(",")
-            mov = Movie(features[0], features[1])
+            mov = Movie(features[0],features[1],features[2],features[3],features[4])
+            movies[mov.title] = mov
 
 
 def add_movie(movie):
-    movies[movie.name] = movie
-    with open("info212\movies.txt", "a") as m:
+    movies[movie.title] = movie
+    with open("info212/movies.txt", "a") as m:
         m.write(movie.info_string()+"\n")
 
 def remove_movie(movie_name):
@@ -40,9 +44,12 @@ def print_movies():
     for movie in movies:
         print(movies[movie].info_string())
 
-
 load_movies()
-print_movies()
+print(movies)
+print(movies)
+save_movies()
+
+
 
 
 "ok"
